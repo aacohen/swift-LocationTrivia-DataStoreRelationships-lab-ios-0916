4375b92fd205c2ef9438419adc16511a1858b3c2
Ariela Cohen <arielacohen@Arielas-MacBook-Air.local>
2016-10-26 15:32:19 -0400

    break. at number 12

---
+++ swift-LocationTrivia-DataStoreRelationships-lab/LocationsTableViewController.swift
@@ -0,0 +1,114 @@
+//
+//  LocationsTableViewController.swift
+//  
+//
+//  Created by Ariela Cohen on 10/26/16.
+//
+//
+
+import UIKit
+
+class LocationsTableViewController: UITableViewController {
+    
+    let store: LocationDataStore = LocationDataStore.sharedInstance
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.accessibilityLabel = "Locations Table"
+        self.tableView.accessibilityIdentifier = "Locations Table"
+        self.navigationItem.rightBarButtonItem?.accessibilityLabel = "addButton"
+        self.navigationItem.rightBarButtonItem?.accessibilityIdentifier = "addButton"
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return store.locations.count
+    }
+
+    
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+
+        let currentLocation = store.locations[indexPath.row]
+        cell.textLabel?.text = currentLocation.name
+        
+        cell.detailTextLabel?.text = String(currentLocation.trivia.count)
+
+        return cell
+    }
+    
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "rightDetailCell" {
+            let destVC = segue.destination as! TriviaTableViewController
+                
+                if let indexPath = self.tableView.indexPathForSelectedRow {
+                    let selectedLocation = store.locations[indexPath.row]
+                    destVC.location = selectedLocation
+                }
+                
+            }
+        }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
